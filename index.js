b6b7a78697809f13ae6a6e0704a9e712c4d0c6d8
Kote <144833957+YakinFsilva@users.noreply.github.com>
2023-10-02 21:17:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display')
+const displayCSS = window.getComputedStyle(display)
+display.style.maxWidth = displayCSS.width
+
+function insert(num) {
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean() {
+  document.getElementById('resultado').innerHTML = "";
+}
+function back() {
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+function calcular() {
+  var resultado = document.getElementById('resultado').innerHTML;
+  if (resultado) {
+    document.getElementById('resultado').innerHTML = eval(resultado);
+  }
+  else {
+    document.getElementById('resultado').innerHTML = "Nada..."
+  }
+
+
+}
